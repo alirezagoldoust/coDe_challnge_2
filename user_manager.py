e2feb4449df70e720b238bc2e086623fed91c331
Borna <borna.aminiraouf@gmail.com>
2026-02-27 11:30:31 +0330

    Created user_manager class

---
+++ user_manager.py
@@ -0,0 +1,9 @@
+class User_manager:
+    def __init__(self):
+        self.user = []
+    def search_user(self, username): -> User
+        pass
+    def add_user(self, User): -> bool
+        pass
+    def duplicate_user(self, User): -> bool
+        pass
